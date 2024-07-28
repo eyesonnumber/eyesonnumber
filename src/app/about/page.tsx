ea013c3e07b7dec820f6e7e23feb2109103523c8
saket1000 <saketjayashwal@gmail.com>
2024-07-28 18:01:44 +0530

    remove comma

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,26 +5,25 @@ const About = () => {
 
       <p className="text-brand-400 w-[70dvw] mt-8">
         Welcome to EyesonNumber, where the stars align to bring clarity and
-        insight into your life's journey. At EyesonNumber, we are passionate
-        about helping you understand the cosmic influences that shape your
-        destiny and empower you to make informed decisions for a brighter
-        future.Our mission is to provide you with the most accurate and
-        personalized numerology and astrology readings, helping you navigate the
-        complexities of life with confidence and wisdom. We believe that
-        everyone deserves to know their unique path and potential, and we are
-        here to guide you every step of the way. Our Services Personalized
-        Readings: Our experienced astrologers and numerologists offer in-depth
-        analyses tailored to your individual needs. Whether you're seeking
-        insights into your career, relationships, health, or personal growth,
-        our readings are designed to provide clarity and direction. Future
-        Planning: Life is a journey, and we are here to help you map out your
-        path. With our comprehensive future planning services, you can set
-        achievable goals and make proactive decisions based on the celestial
-        influences that affect your life. Educational Resources: We believe in
-        empowering you with knowledge. Our extensive library of articles,
-        guides, and tutorials will help you understand the principles of
-        numerology and astrology, allowing you to apply these ancient sciences
-        to your everyday life.
+        insight into your life journey. At EyesonNumber, we are passionate about
+        helping you understand the cosmic influences that shape your destiny and
+        empower you to make informed decisions for a brighter future.Our mission
+        is to provide you with the most accurate and personalized numerology and
+        astrology readings, helping you navigate the complexities of life with
+        confidence and wisdom. We believe that everyone deserves to know their
+        unique path and potential, and we are here to guide you every step of
+        the way. Our Services Personalized Readings: Our experienced astrologers
+        and numerologists offer in-depth analyses tailored to your individual
+        needs. Whether you are seeking insights into your career, relationships,
+        health, or personal growth, our readings are designed to provide clarity
+        and direction. Future Planning: Life is a journey, and we are here to
+        help you map out your path. With our comprehensive future planning
+        services, you can set achievable goals and make proactive decisions
+        based on the celestial influences that affect your life. Educational
+        Resources: We believe in empowering you with knowledge. Our extensive
+        library of articles, guides, and tutorials will help you understand the
+        principles of numerology and astrology, allowing you to apply these
+        ancient sciences to your everyday life.
       </p>
 
       <p className="my-4 text-2xl font-bold text-black">Why choose us?</p>
